1c1457a76764e4f98741f80ae07b2c8b3218bfd3
Andre Cunha <andrecunha.usp@gmail.com>
2012-11-18 15:30:26 -0200

    Added verbose mode to BranchAndBound. Minor performance bug fix in BranchAndBound. Added ReverseSimplexComparator to BranchAndBound.

--- src/bb/BranchAndBound.java
+++ src/bb/BranchAndBound.java
@@ -40,9 +40,20 @@ public class BranchAndBound {
 	private SimplexComparator mSimplexComparator;
 
 	/**
+	 * A variable used to reversely compare two Simplex instances. Used in the
+	 * piority queue only.
+	 */
+	private ReverseSimplexComparator mReverseSimplexComparator;
+
+	/**
 	 * Whether this problem has already been solved.
 	 */
 	private boolean mIsSolved;
+
+	/**
+	 * Whether or not to produce verbose output while running.
+	 */
+	private boolean mVerbose;
 
 	/**
 	 * Creates a BB instance.
@@ -54,7 +65,8 @@ public class BranchAndBound {
 	 */
 	public BranchAndBound(Simplex baseRelaxedProblem, boolean[] integerVariables) {
 		mSimplexComparator = new SimplexComparator();
-		mNodesQueue = new PriorityQueue<Simplex>(10, mSimplexComparator);
+		mReverseSimplexComparator = new ReverseSimplexComparator();
+		mNodesQueue = new PriorityQueue<Simplex>(10, mReverseSimplexComparator);
 		mRelaxedBaseProblem = baseRelaxedProblem;
 		mIntegerVariables = integerVariables;
 	}
@@ -91,24 +103,48 @@ public class BranchAndBound {
 	 * @return True if problem is feasible and bounded; false otherwise.
 	 */
 	public boolean solve() {
+		if (mVerbose) {
+			System.out.println("### B&B ### Starting Branch and Bound.");
+		}
 		mIsSolved = true;
 
 		if (!mRelaxedBaseProblem.solve()) {
 			/* Relaxed base problem is infeasible. */
+			if (mVerbose) {
+				System.out
+						.println("### B&B ### Relaxed base problem is infeasible; returning.");
+			}
 			return false;
 		}
+
+		if (mVerbose) {
+			System.out
+					.println("### B&B ### Adding initial problem to the queue.");
+		}
+
 		mNodesQueue.add(mRelaxedBaseProblem);
 
 		while (!mNodesQueue.isEmpty()) {
 			Simplex currentProblem = mNodesQueue.remove();
 
+			if (mVerbose) {
+				System.out.println("### B&B ### Analyzing next problem:\n"
+						+ currentProblem.getStandardFormDescription());
+			}
+
 			if (mBestSolution != null
 					&& mSimplexComparator
-							.compare(currentProblem, mBestSolution) < 0) {
+							.compare(currentProblem, mBestSolution) <= 0) {
 				/*
 				 * Problem is feasible, but we already have a better solution.
 				 * Prune by quality.
 				 */
+
+				if (mVerbose) {
+					System.out
+							.println("### B&B ### Problem is feasible, but we already have a better or equal solution. Prune by quality.");
+				}
+
 				continue;
 			}
 
@@ -116,8 +152,15 @@ public class BranchAndBound {
 
 			if (var == -1) {
 				/* Solution is integer. */
+				if (mVerbose) {
+					System.out.println("### B&B ### Solution is integer.");
+				}
 				if (mBestSolution == null) {
 					mBestSolution = currentProblem;
+					if (mVerbose) {
+						System.out
+								.println("### B&B ### First integer solution found.");
+					}
 				} else if (mSimplexComparator.compare(currentProblem,
 						mBestSolution) > 0) {
 					/*
@@ -125,9 +168,21 @@ public class BranchAndBound {
 					 * previously.
 					 */
 					mBestSolution = currentProblem;
+
+					if (mVerbose) {
+						System.out
+								.println("### B&B ### Found a new best integer solution.");
+					}
 				}
 			} else {
 				/* Solution has non-integer variables. We need to branch. */
+
+				if (mVerbose) {
+					System.out
+							.println("### B&B ### Solution has non-integer variables. Branching on variable x"
+									+ var + ".");
+				}
+
 				Simplex leftChild = new Simplex(currentProblem);
 				Simplex rightChild = new Simplex(currentProblem);
 
@@ -139,13 +194,29 @@ public class BranchAndBound {
 				rightChild.addConstraint(a, Simplex.GTE, Math.ceil(value));
 
 				if (leftChild.solve()) {
+					if (mVerbose) {
+						System.out.println("### B&B ### Left child (@ "
+								+ leftChild
+										.getObjectiveValueOfOptimalSolution()
+								+ ") is feasible. Adding to the queue.");
+					}
 					mNodesQueue.add(leftChild);
 				}
 
 				if (rightChild.solve()) {
+					if (mVerbose) {
+						System.out.println("### B&B ### Right child (@ "
+								+ rightChild
+										.getObjectiveValueOfOptimalSolution()
+								+ ") is feasible. Adding to the queue.");
+					}
 					mNodesQueue.add(rightChild);
 				}
 			}
+		}
+
+		if (mVerbose) {
+			System.out.println("### B&B ### Finished Branch and Bound.");
 		}
 
 		return (mBestSolution != null);
@@ -190,28 +261,39 @@ public class BranchAndBound {
 
 	/**
 	 * Returns the relaxed base problem.
+	 * 
 	 * @return the relaxed base problem.
 	 */
 	public Simplex getRelaxedProblem() {
 		return mRelaxedBaseProblem;
 	}
-	
+
+	/**
+	 * Activates or deactivates the verbose mode.
+	 * 
+	 * @param verbose
+	 *            if true, activates the verbose mode; if false, deactivates it.
+	 */
+	public void setVerbose(boolean verbose) {
+		mVerbose = verbose;
+	}
+
 	@Override
 	public String toString() {
 		StringBuilder b = new StringBuilder();
-		
+
 		b.append(mRelaxedBaseProblem.toString());
 		b.append("\n\nInteger variables: ");
-		
+
 		for (int i = 0; i < mIntegerVariables.length; i++) {
 			if (mIntegerVariables[i]) {
 				b.append("x" + (i + 1) + " ");
 			}
 		}
-		
+
 		return b.toString().trim();
 	}
-	
+
 	/**
 	 * @param args
 	 */
@@ -228,21 +310,21 @@ public class BranchAndBound {
 		 * baseRelaxedProblem.addBinaryVariableConstraint(4);
 		 */
 
-		/*
-		 * baseRelaxedProblem.setObjectiveFuntion(new double[] {0, 1, 1},
-		 * Simplex.MAXIMIZE); baseRelaxedProblem.addConstraint(new double[] {2,
-		 * 5}, Simplex.LTE, 16); baseRelaxedProblem.addConstraint(new double[]
-		 * {6, 5}, Simplex.LTE, 30);
-		 */
-
-		baseRelaxedProblem.setObjectiveFuntion(new double[] { 0, 10, 6, 4 },
+		baseRelaxedProblem.setObjectiveFuntion(new double[] { 0, 1, 1.5 },
 				Simplex.MAXIMIZE);
-		baseRelaxedProblem.addConstraint(new double[] { 1, 1, 1 }, Simplex.LTE,
-				100);
-		baseRelaxedProblem.addConstraint(new double[] { 10, 4, 5 },
-				Simplex.LTE, 600);
-		baseRelaxedProblem.addConstraint(new double[] { 2, 2, 6 }, Simplex.LTE,
-				300);
+		baseRelaxedProblem
+				.addConstraint(new double[] { 3, 5 }, Simplex.LTE, 16);
+		baseRelaxedProblem
+				.addConstraint(new double[] { 6, 5 }, Simplex.LTE, 30);
+
+		/*
+		 * baseRelaxedProblem.setObjectiveFuntion(new double[] { 0, 10, 6, 4 },
+		 * Simplex.MAXIMIZE); baseRelaxedProblem.addConstraint(new double[] { 1,
+		 * 1, 1 }, Simplex.LTE, 100); baseRelaxedProblem.addConstraint(new
+		 * double[] { 10, 4, 5 }, Simplex.LTE, 600);
+		 * baseRelaxedProblem.addConstraint(new double[] { 2, 2, 6 },
+		 * Simplex.LTE, 300);
+		 */
 
 		/*
 		 * System.out.println(baseRelaxedProblem); baseRelaxedProblem.solve();
@@ -253,16 +335,19 @@ public class BranchAndBound {
 		boolean[] integerVariables = new boolean[3];
 		Arrays.fill(integerVariables, true);
 
-		BranchAndBound bb = new BranchAndBound(baseRelaxedProblem, integerVariables);
+		BranchAndBound bb = new BranchAndBound(baseRelaxedProblem,
+				integerVariables);
+		bb.setVerbose(true);
 		bb.solve();
+		// baseRelaxedProblem.solve();
 
-		System.out.println(bb.mBestSolution);
+		// System.out.println(Arrays.toString(baseRelaxedProblem.getSolution()));
+		System.out.println(Arrays.toString(bb.getSolution()));
 	}
 }
 
 /**
- * A class used to compare two linear programs, so that we can create a priority
- * queue of them.
+ * A class used to compare two linear programs.
  * 
  * @author Andre Luiz Verucci da Cunha
  * 
@@ -275,5 +360,27 @@ class SimplexComparator implements Comparator<Simplex> {
 		double obj2 = o2.getObjectiveValueOfOptimalSolution();
 
 		return DoubleComparator.compare(obj1, obj2);
+	}
+}
+
+/**
+ * A class used to compare two linear programs, so that we can create a reverse
+ * priority queue of them.
+ * 
+ * @author Andre Luiz Verucci da Cunha
+ * 
+ */
+class ReverseSimplexComparator implements Comparator<Simplex> {
+
+	@Override
+	public int compare(Simplex o1, Simplex o2) {
+		double obj1 = o1.getObjectiveValueOfOptimalSolution();
+		double obj2 = o2.getObjectiveValueOfOptimalSolution();
+
+		/*
+		 * We return the opposite of the comparison so that the priority queue
+		 * is reverse-ordered.
+		 */
+		return -DoubleComparator.compare(obj1, obj2);
 	}
 }
